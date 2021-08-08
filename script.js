71ac804b182ba71cc9c070a90766d26281bb01cf
moshiul-riyad <moshiulriyad@gmail.com>
2021-08-07 22:19:09 -0500

    update info

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 
+
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
 });
+
+
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
