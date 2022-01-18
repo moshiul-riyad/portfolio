87331777652c9a3a6ae67d6e477f7910466c8093
moshiul-riyad <moshiulriyad@gmail.com>
2022-01-18 14:57:20 -0600

    add smooth scrolling

--- script.js
+++ script.js
@@ -15,27 +15,6 @@ window.addEventListener('scroll', function(){
 
 // name animation
 
-// Wrap every letter in a span
-// const textWrapper = document.querySelector('.ml2');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml2 .letter',
-//     scale: [4,1],
-//     opacity: [0,1],
-//     translateZ: 0,
-//     easing: "easeOutExpo",
-//     duration: 950,
-//     delay: (el, i) => 70*i
-//   }).add({
-//     targets: '.ml2',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
-
 
   // Wrap every letter in a span
 const textWrapper = document.querySelector('.ml6 .letters');
@@ -55,6 +34,8 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+
 
 //form
 
